96a7848be1bb4273e606ce978e6b19956360bde6
chen7420 <130425698+chen7420@users.noreply.github.com>
2024-09-27 14:37:45 -0700

    UI for login, verification, popup page
    UI design

--- client/src/login.jsx
+++ client/src/login.jsx
@@ -1,11 +1,27 @@
-
+import './login.css';
 import React from 'react';
 
 function login() {
     return (
-        <div>
-            <h1>Hello from login!</h1>
-        </div>
+    <div className="login-background"> {/* background: login-background */}
+      <div className="login-container"> {/* login framework: login-container */} 
+        <h2 className="login-title">Login</h2> {/* title: login-title */}
+        <form className="login-form"> {/* email and password framework: login-form */}
+          <div className="login-input-container">
+            <label htmlFor="username">Email:</label>
+            <input className="login-input" type="text" id="username" name="username" />
+          </div>
+          <div className="login-input-container">
+            <label htmlFor="password">Password:</label>
+            <input className="login-input" type="password" id="password" name="password" />
+          </div>
+          <div className="forgot-password-link">
+            <a href="/forgot-password">Forgot Password?</a> {/* forgot password link */}
+          </div>
+          <button type="submit" className="login-button">Sign in</button> {/* login button: login-button */}
+        </form>
+      </div>
+    </div>
     );
 };
 
