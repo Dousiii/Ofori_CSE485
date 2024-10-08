fac2a48ecedd65fed4697bfa08d0ddf1e44e4b58
DaDa <hzh3n406@as4 edu>
2024-10-08 16:46:10 +0800

    update

--- client/src/Homepage.jsx
+++ client/src/Homepage.jsx
@@ -1,9 +1,16 @@
 import React from "react";
-import { Col, Row, Form, Input } from "antd";
-import { useNavigate } from "react-router-dom"; // Change to useNavigate
+import { Col, Row, Form, Input, Button, Anchor } from "antd";
+import { DownCircleFilled } from '@ant-design/icons';
 import "./style/homePage.css";
 import image from "./assets/image.png";
 function Homepage() {
+  const { Link } = Anchor;
+  const onFinish = async (e) => {
+    console.log(e)
+  }
+  const aatext = () => {
+    return <DownCircleFilled />
+  }
   return (
     <div className="pageHome">
       <h1 className="title">event title</h1>
@@ -16,11 +23,12 @@ function Homepage() {
             will help you walk with pride, knowing your hair is healthy,
             beautiful, and uniquely yours.
           </div>
-          <div className="buttonSubmit">enroll now</div>
+          <Button className="buttonSubmit">enroll now</Button>
         </div>
         <img className="rightImg" src={image} />
       </div>
-      <div className="flexBox mt50 borderBottom">
+
+      <div className="flexBox mt50 borderBottom"  >
         <img className="rightImg" src={image} />
         <div className="leftText">
           <div>
@@ -41,45 +49,69 @@ function Homepage() {
           src="https://video.shipin520.com/videos/36/08/77/b_Z1mR3n9Nd2Eh1578360877.mp4"
           controls
         ></video>
-        <div className="buttonSubmit">enroll now</div>
+        <Button className="buttonSubmit" >enroll now</Button>
       </div>
       <Form
         className="mt50"
         name="basic"
+        onFinish={onFinish}
         size="large"
         labelCol={{
-          span:8,
+          span: 9,
         }}
         wrapperCol={{
-          span: 12,
+          span: 8,
         }}
       >
         <Row>
-          <Col span={12}>
-            <Form.Item label="Username" name="username">
+          <Col span={24}>
+            <Form.Item label="name" name="name">
               <Input />
             </Form.Item>
           </Col>
-          <Col span={12}>
-            <Form.Item label="Phone" name="username">
+          <Col span={24}>
+            <Form.Item label="phone number" name="phoneNumber" rules={[
+              () => ({
+                validator(_, value) {
+                  if (value && value.length == 10) {
+                    return Promise.resolve();
+                  }
+                  return Promise.reject(new Error('from error'));
+                },
+              }),
+            ]}>
               <Input />
             </Form.Item>
           </Col>
         </Row>
         <Row>
-          <Col span={12}>
-            <Form.Item label="mailbox" name="username">
-              <Input />
-            </Form.Item>
-          </Col>
-          <Col span={12}>
-            <Form.Item label="address" name="username">
+          <Col span={24}>
+            <Form.Item label="email" name="email" rules={[
+              () => ({
+                validator(_, value) {
+                  if (value && value.includes('@hotmail.com')) {
+                    return Promise.resolve();
+                  }
+                  if (value && value.includes('@gmail.com')) {
+                    return Promise.resolve();
+                  }
+                  return Promise.reject(new Error('from error'));
+                },
+              }),
+            ]}>
               <Input />
             </Form.Item>
           </Col>
         </Row>
+        <div className="flexCenterBox ">
+          <Button htmlType="submit" className="buttonSubmit" id="componentsSubmit" >
+            Submit
+          </Button>
+        </div>
       </Form>
-      <div className="buttonSubmit" >enroll now</div>
+      <Anchor>
+        <Link href="#componentsSubmit" className="fixedButton" title={aatext()}/>
+      </Anchor>
     </div>
   );
 }
